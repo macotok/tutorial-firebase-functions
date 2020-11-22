31a20f7e3ef4efa986399d008e208e8087e8278c
macotok <monochromer@r8.ucom.ne.jp>
2020-11-22 18:04:27 +0900

    error notification

--- public/js/requests.js
+++ public/js/requests.js
@@ -7,7 +7,7 @@ var app = new Vue({
     upvoteRequest(id) {
       const upvote = firebase.functions().httpsCallable('upvote');
       upvote({ id }).catch((error) => {
-        console.log(error.message);
+        showNotification(error.message);
       });
     },
   },
